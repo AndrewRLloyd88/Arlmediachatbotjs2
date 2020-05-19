479b057588cf038bbd0f4ac32faed2c08171c9ad
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-05-19 16:03:55 -0700

    Added Procfile

--- client/app.js
+++ client/app.js
@@ -1,14 +1,24 @@
-var socket = io();
+let socket = io();
 
 $("button").on('click', function() {
-  var text = $("#message").val();
-  var who = $("#initials").val();
+  let text = $("#message").val();
+  let who = $("#initials").val();
+  let time = new Date().toLocaleTimeString();
   
-  socket.emit('message', who + ": " + text);
+  socket.emit('message', time + ": " + who + " says: " + text);
   $('#message').val('');
-  
   return false;
 });
+
+//update previously missed messages from the server
+socket.on('update messages', function(data){
+//    console.log(data);
+    let i;
+    for (i=0; i<data.length; i++){
+ $('<li>').text(data[i]).appendTo('#history');
+    }
+})
+
 
 socket.on('message', function (msg) {
   $('<li>').text(msg).appendTo('#history');
--- server.js
+++ server.js
@@ -1,21 +1,64 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
-var http = require('http');
-var server = http.Server(app);
+let http = require('http');
+let server = http.Server(app);
+let messageHistory = [];
+let client = [];
+let chatbotWeather = "!Chatbot is pulling information from https://www.metaweather.com/9807/";
 
 app.use(express.static('client'));
 
-var io = require('socket.io')(server);
+app.use(express.static('Client'))
+app.get('/', (req, res) => res.render('Client/index'))
+
+let io = require('socket.io')(server);
+
+function isQuestion(msg) {
+  return msg.match(/!Chatbot/)
+}
+
+function askingWeather(msg) {
+ return msg.match(/weather/i)
+}
+
+function getWeather(callback) {
+ let request = require('request');
+ request.get("https://www.metaweather.com/api/location/9807/", function (error, response) {
+ if (!error && response.statusCode == 200) {
+ let data = JSON.parse(response.body);
+     callback('Todays weather outside is: ' + data.consolidated_weather[0].weather_state_name + '. ' + " Max Temperature is:" + data.consolidated_weather[0].max_temp + "°C" + " Min Temperature is:" + data.consolidated_weather[0].min_temp + "°C");
+ }
+ });
+};
 
 io.on('connection', function (socket) {
-  
+     client.push({id : socket.client.id});
+//    console.log(client);
+    // I don't understand e?
+    let getClientID = client.find(e => (e.id === socket.client.id))
+//       console.log("the Client", getClientID)
+       if(getClientID){
+           socket.emit("update messages", messageHistory);
+      }
+    
   socket.on('message', function (msg) {
     console.log('Received Message: ', msg);
-    io.emit('message', msg);
+    if (!isQuestion(msg)){
+        io.emit('message', msg);
+        messageHistory.push(msg);
+    } else if (askingWeather(msg)) {
+        getWeather(function(weather){
+        io.emit('message', weather);
+        messageHistory.push(weather);
+        io.emit('message', chatbotWeather);
+        messageHistory.push(chatbotWeather);
+        });
+    }; 
   });
 });
 
-server.listen(8080, function() {
+let port = process.env.PORT;
+server.listen(port, function() {
   console.log('Chat server running');
 });
